eff4e9859862fbc4c2d3243c8de440f4430d5b5f
杨智渊 <523120845@qq.com>
2022-04-02 16:04:54 +0800

    提交

---
+++ index.js
@@ -0,0 +1,16 @@
+// $(function () {
+//     $.ajax({
+//         url: 'https://api.muxiaoguo.cn/api/sjbz',
+//         method: 'get',
+//         type: 'json',
+//         data: {
+//             method: 'pc',
+//             type: 'sina'
+//         },
+//         success: function(res){
+//             console.log(res);
+//             $("#back_pic").attr("src", res.data.imgurl);
+//         }
+//     })
+
+// })
